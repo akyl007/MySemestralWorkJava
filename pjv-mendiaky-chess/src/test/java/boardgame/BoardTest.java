83d299c0dfe72055094e7bc02c2aec811f77e0c4
Akylbek Mendibayev <mendiaky@fel.cvut.cz>
2021-05-27 03:26:45 +0500

    adding tests

---
+++ pjv-mendiaky-chess/src/test/java/boardgame/BoardTest.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+import application.UI;
+import chess.ChessMatch;
+import chess.ChessPosition;
+import static org.junit.Assert.*;
+
+import boardgame.BoardException;
+import boardgame.Position;
+import org.junit.Test;
+
+public class BoardTest {
+    @Test
+    public void removePieceCheck(){
+        ChessMatch cm = new ChessMatch();
+        ChessPosition chessPosition = UI.readChessPositionString("b2");
+        Position pos = chessPosition.toPosition();
+        cm.board.removePiece(pos);
+        UI.printBoard(cm.getPieces());
+        assertFalse(cm.board.thereIsAPiece(pos));
+    }
+
+    @Test
+    public void positionExistsTest(){
+        ChessMatch cm = new ChessMatch();
+        ChessPosition chessPosition = UI.readChessPositionString("a2");
+        Position pos = chessPosition.toPosition();
+        assertTrue(cm.board.positionExists(pos));
+
+    }
+}
